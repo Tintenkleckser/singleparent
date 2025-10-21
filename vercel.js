8b233cd27340d866adcbe3b8470d545eff901873
Dr. Peter Westebbe <8150973+Tintenkleckser@users.noreply.github.com>
2025-10-21 18:46:10 +0200

    Add Vercel rewrites for help endpoint
    Add rewrites configuration for help routes

---
+++ vercel.js
@@ -0,0 +1,13 @@
+// vercel.json
+{
+  "rewrites": [
+    {
+      "source": "/help",
+      "destination": "https://chatbase.co/UxjfOj3uIYXEuBxIeH1av/help"
+    },
+    {
+      "source": "/help/:path*",
+      "destination": "https://chatbase.co/UxjfOj3uIYXEuBxIeH1av/help/:path*"
+    }
+  ]
+}
